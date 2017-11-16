033022246e72dd4675a9a23e4562282f5eada05b
Bram Zandbelt <bramzandbelt@gmail.com>
2017-11-16 10:41:46 +0100

    Add required argument to R command

--- pbs/pbs_job_param_recovery.sh
+++ pbs/pbs_job_param_recovery.sh
@@ -3,4 +3,4 @@
 #PBS -o /home/control/brazan/itchmodel/pbs/output/pbs_job_optim_x_$PBS_JOBID.out
 #PBS -j oe
 
-R < /home/control/brazan/itchmodel/R/parameter_recovery_wrapper.R
+R < /home/control/brazan/itchmodel/R/parameter_recovery_wrapper.R --no-save
