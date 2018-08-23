1e786b9ad219d5178578cc37082c6ff2db94839f
Bram Zandbelt <bramzandbelt@gmail.com>
2018-08-23 12:17:55 +0200

    Add settings for running R on DCCN cluster

--- pbs/pbs_job_param_recovery.sh
+++ pbs/pbs_job_param_recovery.sh
@@ -3,4 +3,8 @@
 #PBS -o /home/control/brazan/itchmodel/pbs/output/pbs_job_optim_x_$PBS_JOBID.out
 #PBS -j oe
 
+# Load R 3.3.3
+module unload R
+module load R/3.3.3
+
 R < /home/control/brazan/itchmodel/R/parameter_recovery_wrapper.R --no-save
