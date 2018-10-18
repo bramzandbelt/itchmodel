8712aace0ecb894dc2e1b9314bb2e61bd3393c1f
Bram Zandbelt <bramzandbelt@gmail.com>
2018-10-18 06:42:37 +0200

    Adjusted values for loose bounds, again

--- R/model_functions.R
+++ R/model_functions.R
@@ -910,8 +910,8 @@ get_par_bounds = function(model = "DFT_C", parameterization = "", bound = "lower
 
     lowers = list('alpha' = .01, # DB_JEPG_2014
                   'mu' = 1, # SR_JEPLMC_2013
-                  'mu_gain' = 0,
-                  'mu_loss' = 0, # SR_JEPLMC_2013
+                  'mu_gain' = .01,
+                  'mu_loss' = .01, # SR_JEPLMC_2013
                   'beta' = .01, # DB_JEPG_2014
                   'kappa' = 1, # SR_JEPLMC_2013
                   'kappa_gain' = 0.01,
